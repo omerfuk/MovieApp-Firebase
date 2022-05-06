db2d2f5b713875a142d5a635f62699dac71455e0
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-06 20:43:27 +0300

    .

--- FilmlerUygulamasi/ViewController.swift
+++ FilmlerUygulamasi/ViewController.swift
@@ -7,14 +7,86 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var kategoriTableView: UITableView!
+    
+    var kategorilerListe = [Kategoriler]()
+    
+    var ref:DatabaseReference!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        kategoriTableView.delegate = self
+        kategoriTableView.dataSource = self
+        
+        ref = Database.database().reference()
+       
+        tumKategorilerAl()
+        
+        
+    }
+    
+    func tumKategorilerAl(){
+        
+        ref.child("kategoriler").observe(.value, with: { snapshot in
+            
+            if let gelenVeriButunu = snapshot.value as? [String:AnyObject]{
+                self.kategorilerListe.removeAll()
+                
+                for gelenSatirVerisi in gelenVeriButunu {
+                    if let sozluk = gelenSatirVerisi.value as? NSDictionary{
+                        let key = gelenSatirVerisi.key
+                        let kategori_ad = sozluk["kategori_ad"] as? String ?? ""
+                        
+                        let kategori = Kategoriler(kategori_id: key, kategori_ad: kategori_ad)
+                        self.kategorilerListe.append(kategori)
+                        
+                    }
+                }
+            }
+            
+            DispatchQueue.main.async {
+                self.kategoriTableView.reloadData()
+            }
+            
+        })
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let indeks = sender as? Int
+        
+        let gidilecekVC = segue.destination as! FilmViewController
+        gidilecekVC.kategori = kategorilerListe[indeks!]
     }
 
 
 }
 
+extension ViewController:UITableViewDelegate,UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return kategorilerListe.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let kategori = kategorilerListe[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "kategoriHucre", for: indexPath) as! KategoriHucreTableViewCell
+        
+        cell.labelKategoriAd.text = kategori.kategori_ad
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "toFilm", sender: indexPath.row)
+    }
+}
